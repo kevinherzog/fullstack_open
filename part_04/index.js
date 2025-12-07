2e3104013321978270246830c55ce380e72b0e42
Kevin Herzog <quotenslav@gmail.com>
2025-12-07 12:28:18 +0100

    4.1

--- part_04/index.js
+++ part_04/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 
@@ -12,7 +13,7 @@ const blogSchema = mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { family: 4 })
 
 app.use(express.json())
