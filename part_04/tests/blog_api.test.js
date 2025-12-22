3cb150302f7f2bcf897d05b5b156d4771a3d1d2a
Kevin Herzog <quotenslav@gmail.com>
2025-12-22 21:22:47 +0100

    prev 4.8, this 4.9

--- part_04/tests/blog_api.test.js
+++ part_04/tests/blog_api.test.js
@@ -21,6 +21,18 @@ test.only('all notes are returned', async () => {
   assert.strictEqual(response.body.length, 2)
 })
 
+test.only('is id, not _id', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  response.body.forEach(blog => {
+    assert.ok(blog.id)   
+  })
+})
+
+
 after(async () => {
   await mongoose.connection.close()
 })
