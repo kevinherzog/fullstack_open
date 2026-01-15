fbd30d2dad164c65e1a28069da92d1a8c12ea2d1
Kevin Herzog <quotenslav@gmail.com>
2026-01-15 12:51:00 +0100

    4.19

--- part_04/controllers/blog.js
+++ part_04/controllers/blog.js
@@ -1,6 +1,15 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/users");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -10,9 +19,12 @@ blogRouter.get("/", async (request, response) => {
 blogRouter.post("/", async (request, response, next) => {
   const body = request.body;
 
-  const user = body.userId
-    ? await User.findById(body.userId)
-    : await User.findOne({});
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
   if (!user) {
     return response.status(400).json({ error: "userId missing or not valid" });
   }
